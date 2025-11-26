3c8286df01dd3d76d5777d0b4b9515368831e8c4
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2025-11-26 15:10:55 +0100

    Jointures simples.

--- sql/queries.sql
+++ sql/queries.sql
@@ -16,3 +16,34 @@ FROM products AS p
 INNER JOIN categories AS c
 ON p.id_category = c.id_category
 WHERE c.name_category ILIKE '_lectronique';
+
+-- 4.1
+SELECT p.name_product, p.price, p.stock, c.name_category
+FROM products as p
+INNER JOIN categories AS c
+ON p.id_category = c.id_category;
+
+-- 4.2
+SELECT o.order_ts, o.status_order, CONCAT_WS(' ', c.first_name, c.last_name) AS full_name
+FROM orders AS o
+INNER JOIN customers AS c
+ON o.id_customer = c.id_customer;
+
+-- 4.3
+SELECT
+    CONCAT_WS(' ', c.first_name, c.last_name) AS full_name,
+    p.name_product,
+    oi.quantity,
+    oi.price
+FROM order_items as oi
+INNER JOIN orders as o
+ON oi.id_order = o.id_order
+INNER JOIN products AS p
+ON oi.id_product = p.id_product
+INNER JOIN customers as c
+ON o.id_customer = c.id_customer;
+
+-- 4.4
+SELECT status_order
+FROM orders
+WHERE status_order IN ('PAID', 'SHIPPED');
