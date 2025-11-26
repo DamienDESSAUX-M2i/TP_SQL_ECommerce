aff73be314dedc9b02274193c539813fe39f91fa
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2025-11-26 13:57:31 +0100

    Création des tables.

---
+++ sql/init_pgdb.sql
@@ -0,0 +1,46 @@
+CREATE TABLE categories(
+   id_categorie uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+   name_categorie VARCHAR(255) NOT NULL UNIQUE,
+   description_categorie TEXT
+);
+
+CREATE TABLE products(
+   id_product uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+   name_product VARCHAR(255) NOT NULL,
+   price DECIMAL(10,2),
+   CONSTRAINT price_strictly_positive CHECK (price > 0),
+   stock INT,
+   CONSTRAINT stock_positive CHECK (stock >= 0),
+   id_categorie uuid NOT NULL,
+   CONSTRAINT fk_id_categorie FOREIGN KEY(id_categorie) REFERENCES categories(id_categorie)
+);
+
+CREATE TABLE clients(
+   id_client uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+   first_name VARCHAR(255) NOT NULL,
+   last_name VARCHAR(255) NOT NULL,
+   email VARCHAR(255) NOT NULL UNIQUE,
+   registration_ts TIMESTAMP NOT NULL
+);
+
+CREATE TYPE status_enum AS ENUM ('PENDING', 'PAID', 'SHIPPED', 'CANCELLED');
+
+CREATE TABLE orders(
+   id_order uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+   order_ts TIMESTAMP NOT NULL,
+   status status_enum NOT NULL,
+   id_client uuid NOT NULL,
+   CONSTRAINT fk_id_client FOREIGN KEY(id_client) REFERENCES clients(id_client)
+);
+
+CREATE TABLE order_items(
+   id_order_item uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+   id_product uuid,
+   CONSTRAINT kf_id_product FOREIGN KEY(id_product) REFERENCES products(id_product),
+   id_order uuid,
+   CONSTRAINT kf_id_order FOREIGN KEY(id_order) REFERENCES orders(id_order),
+   quantity INT NOT NULL,
+   CONSTRAINT quantity_strictly_positive CHECK (quantity > 0),
+   price DECIMAL(10,2),
+   CONSTRAINT price_strictly_positive CHECK (price > 0)
+);
