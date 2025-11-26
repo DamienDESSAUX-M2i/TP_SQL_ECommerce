64cf14effdccb4d40e188ffa77ca189c17fdc2b8
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2025-11-26 14:09:49 +0100

    Ajout de "IF NOT EXISTS" à la création des tables.

--- sql/init_pgdb.sql
+++ sql/init_pgdb.sql
@@ -1,10 +1,10 @@
-CREATE TABLE categories(
+CREATE TABLE IF NOT EXISTS categories(
    id_categorie uuid DEFAULT gen_random_uuid() PRIMARY KEY,
    name_categorie VARCHAR(255) NOT NULL UNIQUE,
    description_categorie TEXT
 );
 
-CREATE TABLE products(
+CREATE TABLE IF NOT EXISTS products(
    id_product uuid DEFAULT gen_random_uuid() PRIMARY KEY,
    name_product VARCHAR(255) NOT NULL,
    price DECIMAL(10,2),
@@ -15,7 +15,7 @@ CREATE TABLE products(
    CONSTRAINT fk_id_categorie FOREIGN KEY(id_categorie) REFERENCES categories(id_categorie)
 );
 
-CREATE TABLE clients(
+CREATE TABLE IF NOT EXISTS clients(
    id_client uuid DEFAULT gen_random_uuid() PRIMARY KEY,
    first_name VARCHAR(255) NOT NULL,
    last_name VARCHAR(255) NOT NULL,
@@ -23,17 +23,16 @@ CREATE TABLE clients(
    registration_ts TIMESTAMP NOT NULL
 );
 
-CREATE TYPE status_enum AS ENUM ('PENDING', 'PAID', 'SHIPPED', 'CANCELLED');
-
-CREATE TABLE orders(
+CREATE TABLE IF NOT EXISTS orders(
    id_order uuid DEFAULT gen_random_uuid() PRIMARY KEY,
    order_ts TIMESTAMP NOT NULL,
-   status status_enum NOT NULL,
+   status_order VARCHAR(255) NOT NULL,
+   CONSTRAINT status_order_enum CHECK (status_order in ('PENDING', 'PAID', 'SHIPPED', 'CANCELLED')),
    id_client uuid NOT NULL,
    CONSTRAINT fk_id_client FOREIGN KEY(id_client) REFERENCES clients(id_client)
 );
 
-CREATE TABLE order_items(
+CREATE TABLE IF NOT EXISTS order_items(
    id_order_item uuid DEFAULT gen_random_uuid() PRIMARY KEY,
    id_product uuid,
    CONSTRAINT kf_id_product FOREIGN KEY(id_product) REFERENCES products(id_product),
